a2e69023b131516c1988a593c2d2b1036064eda1
haafizh hamda <haafizhhamda@student.telkomuniversity.ac.id>
2021-06-11 20:59:44 +0700

    initial commit

--- app/src/main/java/org/d3if4076/modul4/MainActivity.kt
+++ app/src/main/java/org/d3if4076/modul4/MainActivity.kt
@@ -1,20 +1,22 @@
 package org.d3if4076.modul4
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivity", "Jumlah data: " + getData().size)
-
     }
-    private fun getData(): List<Hewan> {
+
+    private fun getData(): List<Hewan>{
         return listOf(
-                Hewan("Angsa", "Cygnus olor"),
-                Hewan("Ayam", "Gallus gallus"),
+                Hewan("Angsa","Cygnus olor"),
+                Hewan("Ayam","Gallus gallus"),
                 Hewan("Bebek", "Cairina moschata"),
                 Hewan("Domba", "Ovis ammon"),
                 Hewan("Kalkun", "Meleagris gallopavo"),
@@ -22,8 +24,8 @@ class MainActivity : AppCompatActivity() {
                 Hewan("Kelinci", "Oryctolagus cuniculus"),
                 Hewan("Kerbau", "Bubalus bubalis"),
                 Hewan("Kuda", "Equus caballus"),
-                Hewan("Sapi", "Bos taurus"),
-        )
-    }
+                Hewan("Sapi", "Bos taurus")
 
+                )
+    }
 }
