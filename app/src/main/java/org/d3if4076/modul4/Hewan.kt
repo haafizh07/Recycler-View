46b705b6a2709f84cfe66ca72f0d1f14c2a5642d
haafizh hamda <haafizhhamda@student.telkomuniversity.ac.id>
2021-06-18 01:00:21 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4076/modul4/Hewan.kt
+++ app/src/main/java/org/d3if4076/modul4/Hewan.kt
@@ -3,7 +3,7 @@ package org.d3if4076.modul4
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 
 )
 
--- app/src/main/java/org/d3if4076/modul4/network/HewanApiService.kt
+++ app/src/main/java/org/d3if4076/modul4/network/HewanApiService.kt
@@ -1,18 +1,26 @@
 package org.d3if4076.modul4.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if4076.modul4.Hewan
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://dif.indraazimi.com/"
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
+
 interface HewanApiService {
     @GET("listhewan.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 object HewanApi {
     val service: HewanApiService by lazy {
