c75cbde36643add114e9fc4d0dd6aeb94f7ebf2b
haafizh hamda <haafizhhamda@student.telkomuniversity.ac.id>
2021-06-18 01:10:56 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/org/d3if4076/modul4/Hewan.kt
+++ app/src/main/java/org/d3if4076/modul4/Hewan.kt
@@ -3,7 +3,7 @@ package org.d3if4076.modul4
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int = R.drawable.angsa
+    val imageId: String
 
 )
 
--- app/src/main/java/org/d3if4076/modul4/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if4076/modul4/ui/main/MainAdapter.kt
@@ -3,8 +3,11 @@ package org.d3if4076.modul4.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.d3if4076.modul4.Hewan
+import org.d3if4076.modul4.R
 import org.d3if4076.modul4.databinding.ListItemBinding
+import org.d3if4076.modul4.network.HewanApi
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     private val data = mutableListOf<Hewan>()
@@ -16,10 +19,15 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
     class ViewHolder(private val binding: ListItemBinding) :
             RecyclerView.ViewHolder(binding.root) {
-        fun bind(hewan: Hewan) = with(binding) {
-            namaTextView.text = hewan.nama
-            latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(hewan.imageResId)
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_baseline_broken_image_24)
+                    .into(imageView)
+            }
         }
     }
 
